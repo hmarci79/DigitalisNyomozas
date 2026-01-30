00b738044067faac27a8dd52d5394ddcc0d0340f
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-01-30 09:06:43 +0100

    Bizonyitek osztaly kesz

--- Bizonyitek.cs
+++ Bizonyitek.cs
@@ -8,5 +8,27 @@ namespace DigitalisNyomozas
 {
 	internal class Bizonyitek
 	{
+		private int azonosito;
+		private string tipus;
+		private string leiras;
+		private int megbizhatosag;
+
+		public Bizonyitek(int azonosito, string tipus, string leiras, int megbizhatosag)
+		{
+			this.azonosito = azonosito;
+			this.tipus = tipus;
+			this.leiras = leiras;
+			this.megbizhatosag = megbizhatosag;
+		}
+
+		public int Azonosito { get => azonosito; set => azonosito = value; }
+		public string Tipus { get => tipus; set => tipus = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+		public int Megbizhatosag { get => megbizhatosag; set => megbizhatosag = value; }
+
+		public override string ToString()
+		{
+			return $"{this.azonosito} ({this.tipus}): {this.leiras}\n\tMegbízhatóság: {this.megbizhatosag}";
+		}
 	}
 }
