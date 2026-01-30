29fb56c45f2ab2d25a64408a0c2aa3dcdb5cddb7
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-01-30 08:59:26 +0100

    Felhasznalo osztaly kesz

---
+++ Felhasznalo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Felhasznalo
+	{
+		private string nev;
+		private int azonosito;
+		private string szerepkor;
+
+		public Felhasznalo(string nev, int azonosito, string szerepkor)
+		{
+			this.nev = nev;
+			this.azonosito = azonosito;
+			this.szerepkor = szerepkor;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Azonosito { get => azonosito; set => azonosito = value; }
+		public string Szerepkor { get => szerepkor; set => szerepkor = value; }
+
+		public override string ToString()
+		{
+			return $"{this.nev}: {this.szerepkor} ({this.azonosito})";
+		}
+	}
+}
