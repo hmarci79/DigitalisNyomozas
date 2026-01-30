b38de80df15e7ad3b94d8197efe9f667eb9257dc
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-01-30 09:52:17 +0100

    BizonyitekKezelo osztaly kesz

--- BizonyitekKezelo.cs
+++ BizonyitekKezelo.cs
@@ -8,5 +8,49 @@ namespace DigitalisNyomozas
 {
 	internal class BizonyitekKezelo
 	{
+		private List<Bizonyitek> bizonyitekok;
+
+		public BizonyitekKezelo()
+		{
+			this.bizonyitekok = new List<Bizonyitek>();
+		}
+
+		internal List<Bizonyitek> Bizonyitekok { get => bizonyitekok; set => bizonyitekok = value; }
+
+		public void Hozzaadas(Bizonyitek b)
+		{
+			if (this.bizonyitekok.Contains(b))
+			{
+				Console.WriteLine("Már van ilyen bizonyíték.");
+			}
+			else
+			{
+				Console.WriteLine("Sikeres hozzáadás!");
+				this.bizonyitekok.Add(b);
+			}
+		}
+
+		public void Torles(Bizonyitek b)
+		{
+			if (!this.bizonyitekok.Contains(b))
+			{
+				Console.WriteLine("Nincs ilyen bizonyíték a listában.");
+			}
+			else
+			{
+				Console.WriteLine("Sikeres törlés!");
+				this.bizonyitekok.Remove(b);
+			}
+		}
+
+		public override string ToString()
+		{
+			string s = "";
+			foreach (var item in bizonyitekok)
+			{
+				s += item.ToString()+"\n";
+			}
+			return s;
+		}
 	}
 }
