ec56ab65151bb9f46cb19b5caeeb852fe8f1dc17
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-01-30 09:25:19 +0100

    Gyanusitott osztaly kesz

--- Gyanusitott.cs
+++ Gyanusitott.cs
@@ -8,5 +8,24 @@ namespace DigitalisNyomozas
 {
 	internal class Gyanusitott
 	{
+		private Szemely gyanusSzemely;
+		private int gyanusitottsag;
+		private string statusz;
+
+		public Gyanusitott(Szemely gyanusSzemely, int gyanusitottsag, string statusz)
+		{
+			this.gyanusSzemely = gyanusSzemely;
+			this.gyanusitottsag = gyanusitottsag;
+			this.statusz = statusz;
+		}
+
+		public int Gyanusitottsag { get => gyanusitottsag; set => gyanusitottsag = value; }
+		public string Statusz { get => statusz; set => statusz = value; }
+		internal Szemely GyanusSzemely { get => gyanusSzemely; set => gyanusSzemely = value; }
+
+		public override string ToString()
+		{
+			return $"{this.gyanusSzemely.ToString()}\n\nGyanúsítottsági szint: {this.gyanusitottsag} - Státusz: {this.statusz}";
+		}
 	}
 }
