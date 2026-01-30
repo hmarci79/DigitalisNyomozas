f4bc41361465ad0d04714b07bedc1d47463d75ba
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-01-30 09:29:14 +0100

    Tanu osztaly kesz

--- Tanu.cs
+++ Tanu.cs
@@ -8,5 +8,24 @@ namespace DigitalisNyomozas
 {
 	internal class Tanu
 	{
+		private Szemely tanuSzemely;
+		private string vallomas;
+		private string datum;
+
+		public Tanu(Szemely tanuSzemely, string vallomas, string datum)
+		{
+			this.tanuSzemely = tanuSzemely;
+			this.vallomas = vallomas;
+			this.datum = datum;
+		}
+
+		public string Vallomas { get => vallomas; set => vallomas = value; }
+		public string Datum { get => datum; set => datum = value; }
+		internal Szemely TanuSzemely { get => tanuSzemely; set => tanuSzemely = value; }
+
+		public override string ToString()
+		{
+			return $"{this.tanuSzemely.ToString()}\n\tVallomás: {this.vallomas} ({this.datum})";
+		}
 	}
 }
