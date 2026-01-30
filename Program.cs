b6df869b2aff61b54390b0330e6dc96df2008052
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-01-30 08:45:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalisNyomozas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
