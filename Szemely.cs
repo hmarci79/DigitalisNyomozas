7e80a43c11c9585b3ab882e9e00f6bfc0c378bc7
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-01-30 09:02:58 +0100

    Szemely osztaly kesz

--- Szemely.cs
+++ Szemely.cs
@@ -8,5 +8,24 @@ namespace DigitalisNyomozas
 {
 	internal class Szemely
 	{
+		private string nev;
+		private int kor;
+		private string megjegyzes;
+
+		public Szemely(string nev, int kor, string megjegyzes)
+		{
+			this.nev = nev;
+			this.kor = kor;
+			this.megjegyzes = megjegyzes;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Kor { get => kor; set => kor = value; }
+		public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
+
+		public override string ToString()
+		{
+			return $"{this.nev} ({this.kor}) - {this.megjegyzes}";
+		}
 	}
 }
